e0adc3643a5724bf40e7383cd66e11ae73c67685
Marcelo Lopes da Silva <marcelolopessilva@gmail.com>
2021-08-15 15:20:00 -0300

    Update server.py

--- server.py
+++ server.py
@@ -3,10 +3,27 @@ from flask import render_template
 from sqlalchemy import desc
 import config
 
+# Lib que permite ler variáveis de ambiente.
+from os import environ
+
+# Lê a variável de ambiente HOST e na sua ausência usa o endereço '0.0.0.0'.
+# Esse endereço com zeros indica ao Flask para aceitar requisições de outras origens.
+# Por default o Flask só aceita requisições da máquina local.
+host = environ.get("HOST", '0.0.0.0')
+
+# Lê a variável de ambiente PORT, pois no servidor é um dado dinâmico.
+port = environ.get("PORT", 5000)
+
+# Imprime os dados para aparecer no log do Heroku.
+print("HOST={} PORT={}".format(host, port))
+
 app = config.connex_app
 
 # Lê arquivo swagger.yml para configurar os endpoints da api
 app.add_api("swagger.yml")
+
+# Essa variável "application" será usada pelo Gunicorn para subir o Flask.
+application = app.wsgi_app
 
 @app.route('/')
 def index():
@@ -20,4 +37,4 @@ def index():
     return render_template('index.html', previsoes=previsoes)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
